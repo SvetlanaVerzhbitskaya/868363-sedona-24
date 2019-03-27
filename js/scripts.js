121534fd77b8375c6ad7f5c9bb070556d8171545
Svetlana <svetlana.verzhbitskaya@gmail.com>
2019-03-27 17:55:16 +0300

    Защита проекта

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,34 @@ var link = document.querySelector(".button-search-hotels");
 
 var popup = document.querySelector(".modal-search");
 
-var date = popup.querySelector("[name=arrival-date]");
+var form = popup.querySelector(".appointment-form");
+
+var arrivaldate = popup.querySelector("[name=arrival-date]");
+
+var leavingdate = popup.querySelector("[name=leaving-date]");
+
+var adults = popup.querySelector("[name=adults]");
+
+var children = popup.querySelector("[name=children]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-show");
-  date.focus();
+  popup.classList.remove("modal-error");
+  popup.classList.add("modal-hidden");
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-error");
+  popup.classList.toggle("modal-show");
+  arrivaldate.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivaldate.value || !leavingdate.value || !adults.value || !children.value) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-    }
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   }
-})
+});
